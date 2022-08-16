b4a7eefadbe07231c3a75f52f277d36a0206b0a6
Parsa Poorsistani <poorsistani13@gmail.com>
2022-08-16 21:33:12 +0430

    roman to integer

---
+++ hash/roman-to-integer.py
@@ -0,0 +1,20 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        nums = {
+            
+            'I':1,
+            'V':5,
+            'X':10,
+            'L':50,
+            'C':100,
+            'D':500,
+            'M':1000
+        }
+        s = s.replace("IV", "IIII").replace("IX", "VIIII")
+        s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
+        s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+        ans = 0
+        for c in s:
+            ans=ans+nums[c]
+        return ans
+        
